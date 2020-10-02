57e1ba336cefee7698c14e134d2f530d7300a6e4
Aliaksandr Zaparozhtsau <mr@Aliaksandrs-MacBook-Pro.local>
2020-10-02 17:38:15 +0300

    refactoring of stars

--- star_intersect.py
+++ star_intersect.py
@@ -7,17 +7,7 @@ from operator import itemgetter as get
 import json
 import math
 
-layout = go.Layout( 
-    width=1000,
-    height=1000,
-    xaxis={"range": [-2, 102]},
-    yaxis={"range": [-2, 102]},
-)
-
-fig = go.Figure(layout=layout)
-
-
-def generate_accurate_star_points(_star):
+def generate_accurate_star_points(_star, accuracy = 3):
     (x_arr, y_arr) = generate_star_points(_star)
 
     accuracy = 3
@@ -35,6 +25,30 @@ def generate_accurate_star_points(_star):
             y_new_arr.append(y + i * dy)
 
     return (x_new_arr,y_new_arr)
+
+def generate_star_points(_star):
+    r_inner = _star["r_inner"]
+    r_outer = _star["r_outer"]
+    phi_0 = _star["phi_0"]
+    x = _star["x"]
+    y = _star["y"]
+
+    x_arr = []
+    y_arr = []
+    step = 72
+
+    for i in range(6):
+        rads = np.deg2rad(step * i + phi_0)
+
+        x_arr.append(-r_outer*np.sin(rads) + x)
+        y_arr.append(r_outer*np.cos(rads) + y)
+
+        rads = np.deg2rad(step * i + 36 + phi_0)
+
+        x_arr.append(-r_inner*np.sin(rads) + x)
+        y_arr.append(r_inner*np.cos(rads) + y)
+
+    return (x_arr, y_arr)
 
 def hard_check(_star1, _star2):
     p1arr = generate_accurate_star_points(_star1)
@@ -61,99 +75,88 @@ def is_stars_intersect(_star1,_star2):
     else:
         return False
 
-def generate_star_points(_star):
-    r_inner = _star["r_inner"]
-    r_outer = _star["r_outer"]
-    phi_0 = _star["phi_0"]
-    x = _star["x"]
-    y = _star["y"]
-
+def get_star_scatter(_star, _type = "simple", accuracy = 3):
     x_arr = []
     y_arr = []
-    step = 72
-
-    for i in range(6):
-        rads = np.deg2rad(step * i + phi_0)
-
-        x_arr.append(-r_outer*np.sin(rads) + x)
-        y_arr.append(r_outer*np.cos(rads) + y)
-
-        rads = np.deg2rad(step * i + 36 + phi_0)
-
-        x_arr.append(-r_inner*np.sin(rads) + x)
-        y_arr.append(r_inner*np.cos(rads) + y)
-
-    return (x_arr, y_arr)
-
-def get_star_scatter(_star):
-    (x_arr, y_arr) = generate_star_points(_star)
+    if _type == "simple": 
+        (x_arr, y_arr) = generate_star_points(_star)
+    if _type == "accurate":
+        (x_arr, y_arr) = generate_accurate_star_points(_star, accuracy)
     return go.Scatter(
         x=x_arr,
         y=y_arr
         )
 
-def get_accurate_star_scatter(_star):
-    (x_arr_new, y_arr_new) = generate_accurate_star_points(_star)
-    return go.Scatter(
-        x=x_arr_new,
-        y=y_arr_new
-    )
+def generate_stars(count):
+    stars = []
+    for i in range(p["star_count"]):
+        stars.append(
+            {
+                "phi_0": random.randint(0, 180),
+                "r_inner": p["r_inner"],
+                "r_outer": p["r_outer"],
+                "x": random.randint(0, p["x_max"]),
+                "y": random.randint(0, p["y_max"])
+            }
+        )
+    return stars
+
+def solve_intersections(_stars):
+    s_intersect = {}
+    for s1 in _stars:
+        s_intersect_list = []
+
+        for s2 in _stars:
+            if s1 != s2 and is_stars_intersect(s1, s2):
+                    s_intersect_list.append(s2)
+
+        s_intersect[json.dumps(s1)] = s_intersect_list
 
 
-stars = []
-star_count = 100
-r_inner = 3
-r_outer = 10
-x_max = 100
-y_max = 100
+    keys = list(s_intersect.keys())
+    random.shuffle(keys)
+    keys = set(keys)
 
-for i in range(star_count):
-    stars.append(
-        {
-            "phi_0": random.randint(0, 180),
-            "r_inner": r_inner,
-            "r_outer": r_outer,
-            "x": random.randint(0, x_max),
-            "y": random.randint(0, y_max)
-        }
-    )
+    final_points = []
+    while keys:
+        k = keys.pop()
+        final_points.append(k)
 
+        for each in s_intersect[k]:
+            s_str = json.dumps(each)
+            if keys and s_str in keys: keys.remove(json.dumps(each))
 
-layout = go.Layout( 
-    width=1000,
-    height=1000,
-    xaxis={"range": [-r_outer, x_max+r_outer]},
-    yaxis={"range": [-r_outer, y_max+r_outer]},
-)
+    return final_points
 
-fig = go.Figure(layout=layout)
-
-s_intersect = {}
-for s1 in stars:
-    s_intersect_list = []
-
-    for s2 in stars:
-        if s1 != s2 and is_stars_intersect(s1, s2):
-                s_intersect_list.append(s2)
-
-    s_intersect[json.dumps(s1)] = s_intersect_list
+def impose_stars_on_figure(_stars, _p, _get_scatter):
+    fig = go.Figure(
+        layout=go.Layout( 
+            width=_p["layout_w"],
+            height=_p["layout_h"],
+            xaxis={"range": [-_p["r_outer"], _p["x_max"]+_p["r_outer"]]},
+            yaxis={"range": [-_p["r_outer"], _p["y_max"]+_p["r_outer"]]},
+            )
+        )
 
 
-keys = list(s_intersect.keys())
-random.shuffle(keys)
-keys = set(keys)
+    for each in stars_without_intersections:
+        star_dict = json.loads(each)
+        fig.add_trace(_get_scatter(star_dict))
+    
+    return fig
+        
+p={
+    "star_count":100,
+    "r_inner":3,
+    "r_outer":10,
+    "x_max":100,
+    "y_max":100,
+    "layout_w":1000,
+    "layout_h":1000,
+}
 
-final_points = []
-while keys:
-    k = keys.pop()
-    final_points.append(k)
-
-    for each in s_intersect[k]:
-        s_str = json.dumps(each)
-        if keys and s_str in keys: keys.remove(json.dumps(each))
-
-for each in final_points:
-    star_dict = json.loads(each)
-    fig.add_trace(get_star_scatter(star_dict))
+stars = generate_stars(p)
+stars_without_intersections = solve_intersections(stars)
+fig = impose_stars_on_figure(stars, p, get_star_scatter)
 
 fig.show()
